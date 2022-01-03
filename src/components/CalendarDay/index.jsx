73d95f73eb534b560391fb18618bf0b94c5e19d4
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-01-03 11:44:45 -0300

    Start adding form for new reminders

--- src/components/CalendarDay/index.jsx
+++ src/components/CalendarDay/index.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.scss';
 
-export function CalendarDay({ day, value }) {
+export function CalendarDay({ day, value, calendarState }) {
+  const [reminders, setReminders] = useState([]);
+
   const isThisMonth = day.format('MMMM') === value.clone().format('MMMM');
   const dayName = day.format('dddd');
   const dayNumber = day.format('D');
   const date = day.format("MM/DD/YYYY");
-  const isWeekendDay = (dayName === 'Saturday' || dayName === 'Sunday')
+  const isWeekendDay = (dayName === 'Saturday' || dayName === 'Sunday');
+
+  useEffect(() => {
+    if (calendarState.length && calendarState[0].date === date) {
+      setReminders([...calendarState[0].reminders])
+    }
+  }, [date, calendarState])
+
+  // function addReminderToCalendarDay(date) {
+  //   const newReminder = {
+  //     content,
+  //     id: generateId()
+  //   }
+
+  //   calendarDayContent.reminders.push(newReminder);
+
+  //   addToCalendar((oldstate) => [...oldstate, calendarDayContent]);
+  //   setReminders((oldstate) => [...oldstate, newReminder])
+  //   setContent('');
+  // }
 
   return (
     <div className={`calendar-day ${isWeekendDay ? 'weekend-day' : ''} ${isThisMonth ? '' : 'not-from-this-month'}`}>
-      <div className='day-container'>
-        {dayNumber}
+      <div className='day-header'>
+        <div className='day-container'>
+          {dayNumber}
+        </div>
+        <div className='controls'>
+        </div>
       </div>
-      <div className='reminders-container'>
-        <p className='reminder'>Get groceries</p>
-        <p className='reminder'>Go to the pharmacy</p>
-        <p className='reminder'>AOSDIh OISAD oAS dOIS dA IOShd OAISD</p>
-        <p className='reminder'>OASID  OSIDOId SAd ASAD oiAS diASOdiASOidASOId AS</p>
-        <p className='reminder'>oIAS HD AOSID AASO IDAOSIdhAISOD AOISHD AOSIdhAOSIdhASOdi</p>
-        <p className='reminder'>Get groceries</p>
-        <p className='reminder'>Go to the pharmacy</p>
-        <p className='reminder'>AOSDIh OISAD oAS dOIS dA IOShd OAISD</p>
-        <p className='reminder'>OASID  OSIDOId SAd ASAD oiAS diASOdiASOidASOId AS</p>
-        <p className='reminder'>oIAS HD AOSID AASO IDAOSIdhAISOD AOISHD AOSIdhAOSIdhASOdi</p>
-      </div>
+      <article className='reminders-container'>
+        {reminders && !!reminders.length && reminders.map((reminder) => {
+          return <p key={reminder + Math.floor(Math.random() * 10000)} className='reminder'>{reminder.content}</p>
+        }) }
+      </article>
     </div>
   )
 }
--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -3,25 +3,86 @@ import { CalendarDay } from '../../components/CalendarDay';
 import './calendar.scss';
 import moment from 'moment';
 import { buildCalendar } from '../../utils/calendarHelpers';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 
 function Calendar(props) {
   const [calendar, setCalendar] = useState([]);
   const [value, setValue] = useState(moment());
+  const [isAddingNew, setIsAddingNew] = useState(false);
+  const [reminderContent, setReminderContent] = useState('');
+  const [calendarState, setCalendarState] = useState([]);
+  const [newReminderDate, setNewReminderDate] = useState(null);
+  const [city, setCity] = useState('');
 
   const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
   useEffect(() => {
     const newCalendar = buildCalendar(value)
     setCalendar(newCalendar)
-  }, [value])
+  }, [value]);
+
+  function generateId() {
+    return Math.floor(Math.random() * 100)
+  }
+
+  function handleAddNewReminder() {
+    setIsAddingNew(true);
+  }
+
+  function handleCloseNewReminderInput() {
+    setIsAddingNew(false);
+  }
+
+  function handleSubmitNewReminder() {
+    console.log('saved')
+    const chosenDate = moment(new Date(newReminderDate)).format('MM/DD/YYYY');
+
+    const calendarStateAtDate = calendarState.find(entry => entry.date === chosenDate);
+
+    if (!calendarStateAtDate) {
+      const newCalendarDayContent = {
+        date: chosenDate,
+        reminders: []
+      }
+
+      newCalendarDayContent.reminders.push({
+        content: reminderContent,
+        id: generateId(),
+        city
+      })
+
+      setCalendarState((oldState) => [...oldState, newCalendarDayContent])
+    } else {
+      setCalendarState((oldState) => [...oldState, {...calendarStateAtDate, reminders:[...calendarStateAtDate.reminders, reminderContent]}])
+    }
+  }
 
   return (
     <div className="container">
       <header>
-        {weekDays.map((day => <p className='week-day'>{day}</p>))}
+        {isAddingNew ?
+            <form onSubmit={(e) => e.preventDefault()}>
+              <button className='close-new-reminder-input' onClick={handleCloseNewReminderInput}>X</button>
+              <label for='add-new-reminder'>Reminder:
+                <input id='add-new-reminder' className='add-new-reminder-input' type='text' value={reminderContent}  maxlength="30" onChange={(e) => setReminderContent(e.target.value)} />
+              </label>
+              <label for='new-reminder-date'>Date:
+                <input id='new-reminder-date' className='new-reminder-date' type='datetime-local' value={newReminderDate} onChange={(e) => setNewReminderDate(e.target.value)} />
+              </label>
+              <label>City:
+                <input id='city' className='city' type='text' value={city} onChange={(e) => setCity(e.target.value)}/>
+              </label>
+              <button type='submit' onClick={() => handleSubmitNewReminder()}>Save</button>
+            </form>
+            :
+            <button className='add-new-reminder-button' onClick={handleAddNewReminder}>+</button>
+          }
       </header>
+      <div className='weekdays'>
+        {weekDays.map((day => <p className='week-day'>{day}</p>))}
+      </div>
       <section className='calendar'>
-        {calendar.length && calendar.map((day) => <CalendarDay day={day} value={value}/>)}
+        {calendar.map((day) => <CalendarDay key={day.format('MM/DD/YYYY')} day={day} value={value} calendarState={calendarState} />)}
       </section>
     </div>
   )
